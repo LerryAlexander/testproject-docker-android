329837cd08b71a8ab8f49bc8f4e4d5618d762c8b
Lerry <33426940+LerryAlexander@users.noreply.github.com>
2021-07-14 16:43:58 -0400

    adding url from google drive to download testproject_agent

--- install-register-testproject-agent.sh
+++ install-register-testproject-agent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -X GET 'https://drive.google.com/u/0/uc?id=18niRvPDh9C98YCMU4KXOss_jkAdTpvp1&export=download' --output ./TestProject_Agent_2.3.7.sh
 printf 'y\nyes\n\nyes\nY' | ./TestProject_Agent_2.3.7.sh
 cd /root/testproject/agent/bin
 AGENT_UUID=REMOTE_AGENT_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
